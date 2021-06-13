962e1d0e315e0de07c9727547ed8f0558d376e75
Hsuan-Le Chen <franky920920+gpg@gmail.com>
2021-06-13 16:26:20 +0800

    [BUG]Remove simple light box

--- js/scripts.js
+++ js/scripts.js
@@ -52,11 +52,6 @@ window.addEventListener('DOMContentLoaded', event => {
         });
     });
 
-    // Activate SimpleLightbox plugin for portfolio items
-    new SimpleLightbox({
-        elements: '#news-report a.news-box'
-    });
-
     resize();
 });
 
